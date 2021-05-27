60ebadb3e1dbdeb1130d5fc17d6113fa8850eda0
garciacelisandres <69256208+garciacelisandres@users.noreply.github.com>
2021-05-27 20:22:56 +0200

    Add deployment files (Express, Docker)

---
+++ server.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const path = require("path");
+const express = require("express");
+const helmet = require("helmet");
+const app = express(); // create express app
+
+// Constants
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+// add middlewares
+app.use(helmet());
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
+app.listen(PORT, HOST);
